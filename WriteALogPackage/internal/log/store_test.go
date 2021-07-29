0623750b6f2719a0617ef687897c8c16f53b3057
Andrey Demidov <ceth@yandex-team.ru>
2021-07-29 23:23:37 +0300

    store

---
+++ WriteALogPackage/internal/log/store_test.go
@@ -0,0 +1,37 @@
+package log
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	write = []byte("hello, world")
+	width = uint64(len(write)) + lenWidth
+)
+
+func TestStore(t *testing.T) {
+	f, err := ioutil.TempFile("", "store_test")
+	require.NoError(t, err)
+	defer os.Remove(f.Name())
+
+	s, err := newStore(f)
+	require.NoError(t, err)
+	testAppend(t, s)
+
+	s, err = newStore(f)
+	require.NoError(t, err)
+
+}
+
+func testAppend(t *testing.T, s *store) {
+	t.Helper()
+	for i := uint64(1); i < 4; i++ {
+		n, pos, err := s.Append(write)
+		require.NoError(t, err)
+		require.Equal(t, pos+n, width*i)
+	}
+}
